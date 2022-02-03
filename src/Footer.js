07c7b8f41f5b411a1bb9f0e1ab07eefa84e7db15
Naresh kumawat <iamnareshkumawat1@gmail.com>
2022-02-03 20:53:54 +1200

    final

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return <>
+  <div className='w-full px-5 py-4 bg-black footer'>
+      <div className='w-[80%] mx-auto'>
+          <h2 className='font-mono text-center text-white'>Designed & Built by Naresh Kumawat</h2>
+      </div>
+  </div>
+  </>;
+};
+
+export default Footer;
